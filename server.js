ad1b119a01a36d777f26d9c61f2ad577800564e7
emtec08 <markusdanz@hotmail.de>
2017-10-29 20:58:38 -0400

    server.js update

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
 
 // provide https certificate
-var options = {
-    key: fs.readFileSync('../ssl/server.key'),
-    cert: fs.readFileSync('../ssl/server.crt')
-};
+//var options = {
+  //  key: fs.readFileSync('../ssl/server.key'),
+    //cert: fs.readFileSync('../ssl/server.crt')
+//};
 
 var server = express();
 
@@ -17,13 +18,16 @@ server.use(function(req, res, next) {
     next();
 });
 
-// handle requests to the server
-server.get('/',function(request,response){
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+
+server.post('/',function(request,response){
     console.log("Server access registerd.");
+    console.log(request.body.post);
 
     // trigger classification
     var exec = require('child_process').exec;
-    var child = exec('java -jar ../jar/server.jar "alcohol trump weed ass fuck"',
+    var child = exec('java -jar jar/server.jar ' + request.body.post,
         function (error, stdout, stderr){
             response.writeHead(200, {'Content-Type': 'text/plain'});
             response.write(stdout);
@@ -37,9 +41,18 @@ server.get('/',function(request,response){
 });
 
 // Create an HTTP service
-http.createServer(server).listen(8888);
+//http.createServer(server).listen(8888);
 
 // Create an HTTPS service
-https.createServer(options, server).listen(443, function(){
-    console.log("Server is running...");
+//https.createServer(options, server).listen(443, function(){
+  //  console.log("Server is running...");
+//});
+
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
